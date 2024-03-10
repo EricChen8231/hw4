bd7a24b5e01b9c72ec401025cff31e437be600e0
Eric Chen <echen823@usc.edu>
2024-03-10 16:24:01 +0000

    done with equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,27 +8,57 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-bool checkleaf(Node *root);
-
+bool checkleaf(Node *root, int height);
+int height(Node*root);
 bool equalPaths(Node * root)
 {
     if (root == nullptr){
         return true;
     }
-    
-    return checkleaf(root->left) && checkleaf(root->right);
+    int height1 = height(root)-1;
+    if (root->left == nullptr){
+        return checkleaf(root->right, height1);
+    }
+    if (root->right == nullptr){
+        return checkleaf(root->left, height1);
+    }
+    return checkleaf(root->left, height1) && checkleaf(root->right, height1);
 }
-bool checkleaf(Node *root)
+
+int height(Node *root)
 {
     if (root == nullptr)
     {
-        return true;
+        return 0; 
     }
-
+    int lheight = height(root->left);
+    int rheight = height(root->right);
+    return std::max(lheight, rheight) + 1;
+}
+bool checkleaf(Node *root, int height)
+{
+    if (root == nullptr)
+    {
+        if (height == 0){
+            return true;
+        }
+        return false;
+    }
+    // if its a leaf node and the height doesnt match return false
     if (root->left == nullptr && root->right == nullptr)
     {
-        return true; 
+        if (height == 1){
+            return true;
+        }
+        return false; 
     }
-    return checkLeafDistances(root->left) && checkLeafDistances(root->right);
+    // if its has only 1 child
+    if (root->left == nullptr){
+        return checkleaf(root->right, height-1);
+    }
+    if (root->right == nullptr){
+        return checkleaf(root->left, height-1);
+    }
+    return checkleaf(root->left, height-1) && checkleaf(root->right, height-1);
 }
 
